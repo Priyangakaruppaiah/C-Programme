f75c39f59cf4466aadf90f4dccd4f16cf81edce5
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-15 18:29:25 +0530

    Day-16 completed

---
+++ 17.pyramid1.c
@@ -0,0 +1,22 @@
+//Pyramid in c
+#include <stdio.h>
+int main()
+{
+    int rows,i,j;
+    printf("Enter the rows:");
+    scanf("%d",&rows);
+    for(i=1;i<=rows;i++){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+/*
+Enter the rows:5
+*
+* *
+* * *
+* * * * 
+* * * * *
+*/
