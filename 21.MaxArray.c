46c0c05e82a439204f483a1336643d3509c2e2ea
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-17 18:40:15 +0530

    Day-18 completed

---
+++ 21.MaxArray.c
@@ -0,0 +1,20 @@
+//Find the maximum value in array
+#include <stdio.h>
+int main()
+{
+   int a[20],i,m,c,n;
+   printf("Enter the elements:");
+   scanf("%d",&n);
+   for(i=0;i<n;i++){
+       scanf("%d",&a[i]);
+   }
+   m=a[0];
+   c=1;
+   while(c<n){
+       if(m<a[c]){
+           m=a[c];
+       }
+       c=c+1;
+   }   
+ printf("maximum value is %d",m);
+}
