96d8223fda068521e82db0501206606e8a3780b4
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-19 21:10:17 +0530

    Day-20 completed

---
+++ 31.FarenToCelcius.c
@@ -0,0 +1,11 @@
+//convert Farenheat to Celcius.
+#include <stdio.h>
+int main()
+{  
+   int f,c;    
+   printf("Enter the number");
+   scanf("%d",&f);
+   c=(f-32)/1.8;
+   printf("celcius is %d",c);
+   
+}
