8559124a3a676f7db19377ac708a20ed9803bf54
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-21 20:02:07 +0530

    Day-22 completed

---
+++ 38.MultiplyNoMulOperator.c
@@ -0,0 +1,15 @@
+// To find Multiplication without * operator
+#include <stdio.h>
+int main()
+{
+    int a,b,c=0,d=0;
+    printf("Enter the number:");
+    scanf("%d",&a);
+     printf("Enter the number:");
+    scanf("%d",&b);
+    while(a>=d){
+        c=c+a;
+        d=d+1;
+    }
+    printf("%d",c);
+}
