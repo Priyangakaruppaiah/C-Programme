7334c261f046bed63160b28081e27b2551cc8698
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-23 17:49:31 +0530

    Day-24 completed

---
+++ 41.AsciiValue.c
@@ -0,0 +1,9 @@
+//Find ASCII value of given character
+#include <stdio.h>
+int main() {  
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    printf("ASCII value of %c = %d", c, c);
+    return 0;
+}
