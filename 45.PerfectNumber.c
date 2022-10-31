043726ddbac8c3735ba3d6461993f61ebe3ed74f
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-31 21:17:56 +0530

    Day-25 completed

---
+++ 45.PerfectNumber.c
@@ -0,0 +1,20 @@
+//Find the given number is perfect or not
+#include <stdio.h>
+void main()  
+{  
+int i = 1, num, Sum = 0;  
+printf(" Enter any number to check Perfect Number \n");  
+scanf("%d", &num);  
+while(i < num )  
+    {  
+        if(num % i == 0)  
+            Sum = Sum + i;  
+        i++; 
+        
+    }  
+    if(Sum == num)  
+        printf("%d is Perfect Number", num);  
+    else  
+        printf(" %d is not a Perfect Number", num);  
+
+}   
