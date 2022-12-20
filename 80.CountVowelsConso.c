4272d7125bf615a3bbcd03967b71a280f1bac224
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-12-20 23:20:33 +0530

    Day-48 completed

---
+++ 80.CountVowelsConso.c
@@ -0,0 +1,41 @@
+/C Program to Count the Number of Vowels, Consonants and so on
+#include <stdio.h>
+int main() {
+  char line[150];
+  int vowels, consonant, digit, space;
+  vowels = consonant = digit = space = 0;
+  printf("Enter a line of string: ");
+  fgets(line, sizeof(line), stdin);
+  for (int i = 0; line[i] != '\0'; ++i) {
+  line[i] = tolower(line[i]);
+
+    if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
+        line[i] == 'o' || line[i] == 'u') {
+      ++vowels;
+    }
+    else if ((line[i] >= 'a' && line[i] <= 'z')) {
+      ++consonant;
+    }
+    else if (line[i] >= '0' && line[i] <= '9') {
+      ++digit;
+    }
+    else if (line[i] == ' ') {
+      ++space;
+    }
+  }
+
+  printf("Vowels: %d", vowels);
+  printf("\nConsonants: %d", consonant);
+  printf("\nDigits: %d", digit);
+  printf("\nWhite spaces: %d", space);
+
+  return 0;
+}
+/*
+OUTPUT:
+Enter a line of string: flower12
+Vowels: 2
+Consonants: 4
+Digits: 2
+White spaces: 0
+/*
